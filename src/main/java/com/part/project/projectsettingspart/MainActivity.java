982ed35804dbb415d03877853aa63a1d62067cec
CodeMagicDragon <codedragon@yandex.ru>
2019-01-10 10:31:02 +0300

    Base Commit

---
+++ src/main/java/com/part/project/projectsettingspart/MainActivity.java
@@ -0,0 +1,25 @@
+package com.part.project.projectsettingspart;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    FragmentTransaction ft;
+    StartFragment startf;
+    CardFragment cardf;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startf = new StartFragment();
+        cardf = new CardFragment();
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.layout_main, cardf);
+        ft.commit();
+    }
+}
