44d37dbd14bb4eaa7167138bee0688cd4baec12d
CodeMagicDragon <codedragon@yandex.ru>
2019-01-12 21:11:54 +0300

    Commit update

--- src/main/java/com/part/project/projectsettingspart/MainActivity.java
+++ src/main/java/com/part/project/projectsettingspart/MainActivity.java
@@ -1,8 +1,11 @@
 package com.part.project.projectsettingspart;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,7 +13,9 @@ public class MainActivity extends AppCompatActivity
 
     FragmentTransaction ft;
     StartFragment startf;
-    CardFragment cardf;
+    ListFragment listf;
+    SettingsFragment settingsf;
+    Button bDownload, bEdit, bSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,9 +23,47 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startf = new StartFragment();
-        cardf = new CardFragment();
+        listf = new ListFragment();
+        settingsf = new SettingsFragment();
         ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.layout_main, startf);
+        bDownload = findViewById(R.id.b_download);
+        bEdit = findViewById(R.id.b_edit);
+        bSettings = findViewById(R.id.b_settings);
+        bDownload.setOnClickListener(click);
+        bEdit.setOnClickListener(click);
+        bSettings.setOnClickListener(click);
         ft.commit();
     }
+
+    private void replaceFragment(Fragment f)
+    {
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.layout_main, f);
+        ft.commit();
+    }
+
+    private final View.OnClickListener click = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            switch (v.getId())
+            {
+                case R.id.b_settings:
+                    replaceFragment(settingsf);
+                    // go to settings
+                    break;
+                case R.id.b_edit:
+                    replaceFragment(listf);
+                    // go to card set list
+                    break;
+                case R.id.b_download:
+                    // go to web part
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
--- src/main/java/com/part/project/projectsettingspart/SettingsFragment.java
+++ src/main/java/com/part/project/projectsettingspart/SettingsFragment.java
@@ -1,19 +1,20 @@
 package com.part.project.projectsettingspart;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CardFragment extends Fragment
+public class SettingsFragment extends Fragment
 {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_card, container,false);
+        return inflater.inflate(R.layout.fragment_settings, container,false);
     }
 }
