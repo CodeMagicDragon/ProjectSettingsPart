87ce581c6eebf375bbaadb67271ec16c8bfede50
CodeMagicDragon <codedragon@yandex.ru>
2019-01-13 09:51:50 +0300

    Commit update

---
+++ src/main/java/com/part/project/projectsettingspart/AppListActivity.java
@@ -0,0 +1,13 @@
+package com.part.project.projectsettingspart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_list);
+    }
+}
